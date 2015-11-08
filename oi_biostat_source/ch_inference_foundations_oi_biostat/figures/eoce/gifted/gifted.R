625b8435a13f6642a9e841c59209d510afd8a20a
Alice Zhao <alicezhao@Alices-MacBook-Pro.local>
2015-11-08 16:10:37 -0500

    exercises

--- oi_biostat_source/ch_inference_foundations_oi_biostat/figures/eoce/gifted/gifted.R
+++ oi_biostat_source/ch_inference_foundations_oi_biostat/figures/eoce/gifted/gifted.R
@@ -1,5 +1,5 @@
 setwd("~/Desktop/Open Intro/EOCE - Second Edition/04/figures/eoce/gifted")
-
+setwd('~/OI_Biostat/oi_biostat_source/ch_inference_foundations_oi_biostat/figures/eoce/gifted/')
 library(openintro)
 data(gifted)
 
@@ -58,3 +58,23 @@ normTail(m = m, s = s, U = u, L = 81.8, axes = FALSE, col = oiB, xlim = c(80,120
 axis(1, at = c(m - 3*s,l,m,u,m + 3*s), label = c(NA,l,m,u,NA), cex.axis = 1.55)
 
 dev.off()
+
+
+########################################
+
+pdf("parentIQ.pdf", height = 3, width = 6)
+par(mar=c(4, 3, 1.4, 1), las=1, mgp=c(3,1,0), mfrow = c(1,2))
+
+histPlot(gifted$motheriq, col = COL[1], xlab = "Mother's IQ", ylab = "", ylim=c(0,14))
+
+par(las = 1, mar = c(4, 3, 1.4, 1))
+histPlot(gifted$fatheriq, col = COL[1], xlab = "Father's IQ", ylab = "", ylim=c(0,14))
+
+dev.off()
+
+#difference CI
+mean(gifted$motheriq)
+mean(gifted$fatheriq)
+
+parent.diff <- gifted$motheriq - gifted$fatheriq
+sd(parent.diff)/sqrt(length(parent.diff))
